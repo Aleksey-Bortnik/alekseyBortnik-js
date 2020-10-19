2188bb02c10d543259bf3ce124267f189f7c58a6
Aleksey <Aleksey.bortnik91@gmail.com>
2020-10-20 00:09:19 +0300

    Task 4 is done.

--- hm5/index.js
+++ hm5/index.js
@@ -20,4 +20,33 @@ function taskOneOnClick() {
 
   alert(sum);
 }
+function taskTwoOnClick() {
+  let divisible1 = +document.getElementById("task-2-first-number").value;
+  let divisible2 = +document.getElementById("task-2-second-number").value;
 
+  let dividers = [];
+
+  for (
+    let divider = 1;
+    divider <= divisible1 && divider <= divisible2;
+    divider++
+  ) {
+    if (divisible1 % divider === 0 && divisible2 % divider === 0) {
+      dividers.push(divider);
+    }
+  }
+  alert(dividers.pop());
+}
+
+function taskFourOnClick() {
+  let taskNumber = +document.getElementById("task-4-number").value;
+  let result = countDigits(taskNumber);
+  alert(result);
+}
+
+function countDigits(n) {
+  for (var i = 0; n > 1; i++) {
+    n /= 10;
+  }
+  return i;
+}
